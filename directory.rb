bc077f79ad33d40a92b4ec9c0e9402e6fb4c9091
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-06-15 14:51:12 +0100

    Added methods to directory

--- directory.rb
+++ directory.rb
@@ -12,10 +12,19 @@ students = [
   "Norman Bates"
 ]
 
-puts "The students of Villians Academy"
-puts "-------------"
+def print_header
+  puts "The students of Villians Academy"
+  puts "-------------"
+end
 
-students.each { |student| puts student }
+def print(names)
+  names.each { |name| puts name }
+end
 
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
+print_header
+print(students)
+print_footer(students)
