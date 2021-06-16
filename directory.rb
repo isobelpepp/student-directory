8cee9d090a5221ed06b2d2440332f69dc5a7ebd2
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-06-16 13:51:07 +0100

    Taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -1,33 +1,5 @@
 @students = []
 
-def input_students
-  puts 'Please enter the names of the students'
-  puts 'To finish just hit return twice'
-
-  name = gets.chomp
-
-  while !name.empty? do
-    @students << { name: name, cohort: :november }
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-
-  @students
-end
-
-def print_header
-  puts 'The students of Villians Academy'
-  puts '-------------'
-end
-
-def print_students_list(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort) " }
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
 def print_menu
   puts '1. Input the students'
   puts '2. Show the students'
@@ -36,29 +8,11 @@ def print_menu
   puts '9. Exit'
 end
 
-def show_students
-  print_header
-  print_students_list(@students)
-  print_footer(@students)
-end
-
-def save_students
-  file = File.open('students.csv', 'w')
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(',')
-    file.puts csv_line
+def interactive_menu
+  loop do
+    print_menu
+    process($stdin.gets.chomp)
   end
-  file.close
-end
-
-def load_students
-  file = File.open('students.csv', 'r')
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << { name: name, cohort: cohort.to_sym }
-  end
-  file.close
 end
 
 def process(selection)
@@ -78,11 +32,69 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish just hit return twice'
+
+  name = $stdin.gets.chomp
+
+  while !name.empty? do
+    @students << { name: name, cohort: :november }
+    puts "Now we have #{@students.count} students"
+    name = $stdin.gets.chomp
   end
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts 'The students of Villians Academy'
+  puts '-------------'
+end
+
+def print_students_list
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort) " }
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  file = File.open('students.csv', 'w')
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(',')
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = 'students.csv')
+  file = File.open(filename, 'r')
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << { name: name, cohort: cohort.to_sym }
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
